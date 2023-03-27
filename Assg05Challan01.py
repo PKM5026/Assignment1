d7fd9db3af10994a2ecc29ad6e6649411506765d
PKM5026 <125748584+PKM5026@users.noreply.github.com>
2023-03-27 20:42:01 +0530

    Add files via upload

---
+++ Assg05Challan01.py
@@ -0,0 +1,19 @@
+class Point:
+
+    def __init__(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def sqSum(self):
+        return (self.x * self.x)+(self.y * self.y)+(self.z * self.z)
+
+
+p = Point(1, 3, 5)
+
+print(p.sqSum())
+
+'''
+Output:
+35
+'''
