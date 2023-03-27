9e3c4f69a3130af6c09c68fe6fb56672c9167087
PKM5026 <125748584+PKM5026@users.noreply.github.com>
2023-03-27 21:19:55 +0530

    Add files via upload

---
+++ Assg05Challan04.py
@@ -0,0 +1,14 @@
+class account:
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance
+
+
+class savings_account(account):
+    def __init__(self, title=None, balance=0, interest_rate=0):
+        super().__init__(title,balance)
+        self.interest_rate = interest_rate
+
+account = savings_account('Kishan',60000,5)
+print(account.title,account.balance,account.interest_rate)
+
